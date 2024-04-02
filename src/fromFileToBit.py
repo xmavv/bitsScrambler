c069b521c041ca4e21a2102fb39075e8ae9b85ec
lipa13 <jaklip04@gmail.com>
2024-04-02 20:27:03 +0200

    commented unnecessary lines

--- src/fromFileToBit.py
+++ src/fromFileToBit.py
@@ -23,25 +23,25 @@ def mp3_to_bytes(file_path):
     return audio_bytes
 
 # path
-wav_file_path = '../samples/wow.wav'
-mp3_file_path = '../samples/wow.mp3'
+# wav_file_path = '../samples/wow.wav'
+# mp3_file_path = '../samples/wow.mp3'
 
 # conversion
 # audio_bytes = wav_to_bytes(wav_file_path)
-audio_bytes = mp3_to_bytes(mp3_file_path)
+# audio_bytes = mp3_to_bytes(mp3_file_path)
 
 def bytes_to_bits(byte_string):
     bits = []
     for byte in byte_string:
-        bits.append(format(byte, '08b'))  #byte to bit, uzupelnia do 8 bitow w kazdej sekwencji
+        bits.append(format(byte, '08b'))  # byte to bit, uzupelnia do 8 bitow w kazdej sekwencji
     return bits
 
-bit_string = bytes_to_bits(audio_bytes) #all bytes
+# bit_string = bytes_to_bits(audio_bytes) #all bytes
 
 # print
-for index, byte_bits in enumerate(bit_string):
-    print(f"Bajt {index}: {byte_bits}")
+# for index, byte_bits in enumerate(bit_string):
+#    print(f"Bajt {index}: {byte_bits}")
 
 # pierwsze 100 bajtow nieskonwertowane (w postaci szesnatskowej)
-print(audio_bytes[:100])
-print("dlugosc ciągu bajtów:", len(bytes_to_bits(audio_bytes)))
+# print(audio_bytes[:100])
+# print("dlugosc ciągu bajtów:", len(bytes_to_bits(audio_bytes)))
