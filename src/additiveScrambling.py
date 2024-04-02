f90d3e0092b1a836083abe35feaa614027d7b17f
xmavv <xxmatheooo@gmail.com>
2024-04-02 22:12:10 +0200

    xor function changed

--- src/additiveScrambling.py
+++ src/additiveScrambling.py
@@ -9,19 +9,14 @@ def generate_sequence(length):
 
 # funckja wykonująca operację xor na dwóch ciągach bitów
 def xor_bit_strings(bit_string1, bit_string2):
-    result = []  # Pusta lista na wynikowe bity
+    result = []
 
-    # Iteracja przez indeksy i wartości bitów obu ciągów
     for i in range(len(bit_string1)):
-        # wykonanie operacji XOR na każdej parze bitów i dodanie wyniku do listy
-        # konwersja bitów na liczby całkowite, wykonanie operacji XOR, a następnie konwersja wyniku z powrotem na ciąg znaków
         result_bit = str(int(bit_string1[i]) ^ int(bit_string2[i]))
         result.append(result_bit)
 
-    # Połączenie wynikowych bitów w jeden ciąg i zwrócenie go
     return ''.join(result)
 
-# funkcja wykonująca addytwną metodę scramblingu
 def scramble_bits(bit_string):
     pseudorandom_sequence = generate_sequence(len(bit_string))
     scrambled_bits = xor_bit_strings(bit_string, pseudorandom_sequence)
@@ -42,8 +37,9 @@ else:
 
 # konwersja bajtów na bity i scramblowanie
 bit_string = bytes_to_bits(audio_bytes)
+# bit_string = '010101010'
 scrambled_bit_string = scramble_bits(bit_string)
 
 # wyniki
-print("Oryginalny ciąg bitów:", bit_string[:100], "...")  # pokaż pierwsze 100 bitów ciągu wejściowego
-print("Scrambled ciąg bitów:", scrambled_bit_string[:100], "...") # pokaż pierwsze 100 bitów ciągu wyjściowego
+print("Oryginalny ciąg bitów:", bit_string[:100])  # pokaż pierwsze 100 bitów ciągu wejściowego
+print("Scrambled ciąg bitów:", scrambled_bit_string[:100]) # pokaż pierwsze 100 bitów ciągu wyjściowego
