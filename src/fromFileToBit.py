906e255d7325bacce9e809729ec4ee51a32fe272
lipa13 <jaklip04@gmail.com>
2024-04-02 18:23:33 +0200

    test commit

--- src/fromFileToBit.py
+++ src/fromFileToBit.py
@@ -2,31 +2,31 @@ import wave
 from pydub import AudioSegment
 
 
-#bigger file, less bits
+# bigger file, fewer bits
 def wav_to_bytes(file_path):
     with wave.open(file_path, 'rb') as wave_file:
-        #pobierz parametry .wav
+        # pobierz parametry .wav
         nchannels = wave_file.getnchannels()
         sampwidth = wave_file.getsampwidth()
         framerate = wave_file.getframerate()
         nframes = wave_file.getnframes()
 
-        #odczytaj zawartosc .wav
+        # odczytaj zawartosc .wav
         frames = wave_file.readframes(nframes)
 
     return frames
 
-#smaller file, compression makes more bits then
+# smaller file, compression makes more bits then
 def mp3_to_bytes(file_path):
     audio = AudioSegment.from_mp3(file_path)
     audio_bytes = audio.raw_data
     return audio_bytes
 
-#path
+# path
 wav_file_path = '../samples/wow.wav'
 mp3_file_path = '../samples/wow.mp3'
 
-#conversion
+# conversion
 # audio_bytes = wav_to_bytes(wav_file_path)
 audio_bytes = mp3_to_bytes(mp3_file_path)
 
@@ -38,10 +38,10 @@ def bytes_to_bits(byte_string):
 
 bit_string = bytes_to_bits(audio_bytes) #all bytes
 
-#print
+# print
 for index, byte_bits in enumerate(bit_string):
     print(f"Bajt {index}: {byte_bits}")
 
-#pierwsze 100bajtow nie zkonwertowane (w postaci szesnatskowej)
+# pierwsze 100 bajtow nieskonwertowane (w postaci szesnatskowej)
 print(audio_bytes[:100])
 print("dlugosc ciągu bajtów:", len(bytes_to_bits(audio_bytes)))
